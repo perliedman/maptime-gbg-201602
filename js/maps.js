b948bca81d2c494658ac12825f2a23d6de37c40d
Per Liedman <per@liedman.net>
2016-02-24 22:35:40 +0100

    Complete layers map

--- js/maps.js
+++ js/maps.js
@@ -37,6 +37,9 @@ window.layersMap = (function() {
                 L.marker([57.6991, 11.9321]).addTo(map);
             },
             function() {
+                L.marker([57.6991, 11.9321], {draggable: true}).addTo(map);
+            },
+            function() {
                 var coords = [[57.6991, 11.9320],[57.6986, 11.9325],[57.6995, 11.9366],[57.6992, 11.9411],[57.7003, 11.9520],[57.6998, 11.9609],[57.7012, 11.9691],[57.7049, 11.9772],[57.7074, 11.9776]],
                     line = L.polyline(coords.slice(0, 2)).addTo(map),
                     counter = 2,
@@ -49,15 +52,66 @@ window.layersMap = (function() {
                     }, 1000);
             },
             function() {
+                var coords = [[57.6991, 11.9320],[57.6986, 11.9325],[57.6995, 11.9366],[57.6992, 11.9411],[57.7003, 11.9520],[57.6998, 11.9609],[57.7012, 11.9691],[57.7049, 11.9772],[57.7074, 11.9776]];
+
+                outline = L.polyline(coords, {color: 'black', opacity: 0.4, weight: 13}).addTo(map);
+                L.polyline(coords, {color: 'white', opacity: 1.0, weight: 11}).addTo(map);
+                L.polyline(coords, {color: 'blue',  opacity: 1.0, weight:  5}).addTo(map);
+            },
+            function() {
+                outline.bringToFront();
+            },
+            function() {
+                outline.bringToBack();
+            },
+            function() {
                 var coords = [[[57.6858, 11.9276],[57.6869, 11.9281],[57.6873, 11.9320],[57.6928, 11.9381],[57.6935, 11.9455],[57.6902, 11.9462],[57.6901, 11.9525],[57.6852, 11.9517],[57.6807, 11.9458],[57.6809, 11.9440],[57.6798, 11.9359],[57.6833, 11.9323],[57.6858, 11.9276]]];
                 L.polygon(coords).addTo(map);
             },
             function() {
                 L.circle([57.7010, 11.9759], 200).addTo(map);
+            },
+            function() {
+                L.circleMarker([57.7010, 11.9759], {className: 'pulse-marker'}).addTo(map);
+                /* 
+                .pulse-marker {
+                    stroke: red;
+                    stroke-width: 2;
+                    stroke-opacity: .5;
+                    fill: red;
+                    fill-opacity: .9;
+                    animation: pulse 2s ease-out;
+                    animation-iteration-count: infinite;
+                }
+
+                @keyframes pulse {
+                    from {
+                        stroke-width: 15;
+                        stroke-opacity: 1
+                    }
+
+                    to {
+                        stroke-width: 50;
+                        stroke-opacity: 0
+                    }
+                }
+                */
+            },
+            function() {
+                L.marker([57.6946, 11.9920])
+                    .addTo(map)
+                    .bindPopup('<h3>Liseberg</h3>');
+            },
+            function() {
+                L.popup()
+                    .setLatLng([57.6983, 11.9712])
+                    .setContent('<h3>Vasaparken</h3>')
+                    .openOn(map);
             }
         ],
         step = 0,
         codeDiv = document.querySelector('#layers-map-code'),
+        outline,
         map;
 
     return {
@@ -72,7 +126,7 @@ window.layersMap = (function() {
                 var fn = steps[step++];
                 if (!fn) return;
                 fn();
-                codeDiv.innerHTML = fn.toString()
+                codeDiv.innerText = fn.toString()
                     .replace(/^            \}$/gm, '')
                     .replace(/                (\S)/gm, '$1')
                     .replace(/^function \(\) \{$/gm, '')
